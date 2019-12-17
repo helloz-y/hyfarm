a19b0104eb28df914acb8847f95b9d68727ba2cc
张洋 <zhangyang@msyizu.com>
2019-12-17 15:52:52 +0800

    初始化用户系统web服务

---
+++ hyfarm-user/hyfarm-user-web/src/main/java/com/hyfarm/HyfarmUserWebApplication.java
@@ -0,0 +1,13 @@
+package com.hyfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyfarmUserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyfarmUserWebApplication.class, args);
+    }
+
+}
---
+++ hyfarm-user/hyfarm-user-web/src/test/java/com/hyfarm/HyfarmUserWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyfarmUserWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
