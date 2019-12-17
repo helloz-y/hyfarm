ea555d8e1d8fea76df5d34b8faaa7881f9b8ec9d
张洋 <zhangyang@msyizu.com>
2019-12-17 17:41:43 +0800

    初始化用户系统service服务

---
+++ hyfarm-user/hyfarm-user-service/src/main/java/com/hyfarm/HyfarmUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hyfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyfarmUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyfarmUserServiceApplication.class, args);
+    }
+
+}
