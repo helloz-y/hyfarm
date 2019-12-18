44b0fc597271c00c39b135e22cfc7f9303db553e
张洋 <zhangyang@msyizu.com>
2019-12-18 17:46:25 +0800

    初始化base项目

---
+++ hyfarm-base/src/main/java/com/hyfarm/HyfarmBaseApplication.java
@@ -0,0 +1,13 @@
+package com.hyfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyfarmBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyfarmBaseApplication.class, args);
+    }
+
+}
