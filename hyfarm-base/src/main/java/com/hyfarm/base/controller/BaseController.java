6fc789df9c0504b025912cf386184574690ff520
张洋 <zhangyang@msyizu.com>
2019-12-18 18:00:02 +0800

    初始化base项目

---
+++ hyfarm-base/src/main/java/com/hyfarm/base/controller/BaseController.java
@@ -0,0 +1,14 @@
+package com.hyfarm.base.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * controller 层的基本父类
+ */
+public class BaseController {
+    /**
+     * 日志对象
+     */
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+}
---
+++ hyfarm-base/src/main/java/com/hyfarm/base/domain/BaseResponse.java
@@ -0,0 +1,8 @@
+package com.hyfarm.base.domain;
+
+import java.io.Serializable;
+
+public class BaseResponse implements Serializable {
+
+    private static final long serialVersionUID = -7161472838749220765L;
+}
